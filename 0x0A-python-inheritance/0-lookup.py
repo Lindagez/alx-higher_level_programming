a32a1dc1e09c6e801b6a6a1f64f2fea11810890a
root <lidiyagez@gmail.com>
2023-01-09 12:28:05 -0800

    python

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"Module 0-lookup.
+Finding all attrubutes and methods of an object and store it in the list.
+"
+def lookup(obj):
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 5-base_geometry.
+Empty class.
+"""
+
+
+class BaseGeometry:
+    """The empty class"""
+
+    pass
